f22166ada29bd3878f88cf84f1e0b2d03423175b
andykhv <khovandyak@gmail.com>
2021-08-13 16:56:10 -0700

    initial commit

---
+++ server/src/main/java/com/andykhov/camillia/CamilliaApplication.java
@@ -0,0 +1,13 @@
+package com.andykhov.camillia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamilliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamilliaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/andykhov/camillia/CamilliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andykhov.camillia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamilliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
